844cd9098680e01d19a32d20778d9c2a0a0b1cc9
Cermaq Bodega <deploy@cermaq.local>
2026-01-28 10:40:55 -0300

    PWA: Finalize branding (Cermaq Bodega + Q.png) and Service Worker

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'cermaq-bodega-v2';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './Q.png',
+    'https://unpkg.com/html5-qrcode',
+    'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
+    'https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
